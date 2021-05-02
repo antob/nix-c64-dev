189e1d68cf44493f87655bd4e84f5eb2158ff200
Maxwell Beck <max@ryt.one>
2021-05-02 16:41:23 -0500

    Remove 3to4 converter from KickAssembler
    Seems to have been removed

--- tools/kickassembler.nix
+++ tools/kickassembler.nix
@@ -6,7 +6,7 @@ pkgs.stdenv.mkDerivation {
 
     src = pkgs.fetchurl {
         url = http://theweb.dk/KickAssembler/KickAssembler.zip;
-        sha256 = "1dbwjabgjaylam68zdvsp880yg6yaydghgllkrg9758mzrpx1yin";
+        sha256 = "lJIwFNysFoRDlnrWNvcHjP6zViK03+rFRySE7RkbnmE=";
     };
 
     nativeBuildInputs = [ pkgs.unzip pkgs.makeWrapper ];
@@ -14,13 +14,9 @@ pkgs.stdenv.mkDerivation {
     sourceRoot = ".";
     installPhase = ''
         install -Dm644 KickAss.jar $out/share/java/KickAss.jar
-        install -Dm644 KickAss3To4Converter.jar $out/share/java/KickAss3To4Converter.jar
 
         makeWrapper ${pkgs.jre}/bin/java $out/bin/kickass \
             --add-flags "-jar $out/share/java/KickAss.jar"
-
-        makeWrapper ${pkgs.jre}/bin/java $out/bin/kickass3to4converter \
-            --add-flags "-jar $out/share/java/KickAss3To4Converter.jar"
     '';
 
     meta = with pkgs.lib; {
